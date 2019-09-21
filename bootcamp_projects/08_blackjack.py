edd23b0dfecb676ddc2b47bf0b5a92eae9c97ac6
nimblejack117@gmail.com <nimblejack117@gmail.com>
2019-09-21 01:32:35 -0700

    Blackjack update
    changed show() -> check_and_show() : now gets points and adjusts Aces correctly
    hit() : now correctly tracks Aces
    ace_value_to_1 : changed logic to be simpler

--- bootcamp_projects/08_blackjack.py
+++ bootcamp_projects/08_blackjack.py
@@ -3,7 +3,8 @@ import random
 class Deck():
     def __init__(self):
         suits = ['Hearts', 'Spades', 'Diamonds', 'Clubs']
-        ranks = [1, 2, 3, 4, 5, 6, 7, 8, 9, 'J', 'Q', 'K', 'A']
+        #ranks = [1, 2, 3, 4, 5, 6, 7, 8, 9, 'J', 'Q', 'K', 'A']
+        ranks = [1,2,3,'K','A']
         self.deck = []
         for suit in suits:
             for rank in ranks:
@@ -32,44 +33,52 @@ class Hand():
         self.aces = 0
 
     def points(self):
+        # calculates the points everytime
+        self.point_value = 0
         for index, card in enumerate(self.cards):
             self.point_value += self.cards[index][1]
 
-    def show(self):
+    def check_and_show(self):
+        # Shows hand in easy to read format
+        # calls points()
+        # adjusts Ace's to 1's if over 21
         current_hand = []
         s = ', '
-        self.point_value = 0
         for card in self.cards:
             current_hand.append(card[0])
         Hand.points(self)
+        while self.point_value > 21 and self.aces > 0:
+            Hand.ace_value_to_1(self)
+            Hand.points(self)
+            self.aces -= 1
         return 'Your hand: %s \nYour points: %i' % (s.join(current_hand),self.point_value)
 
     def hit(self, deck):
+        # draws a card from deck and checks if it's an Ace
         self.cards.append((deck.pop()))
-        if deck.pop()[0] == 'A':
+        if self.cards[-1][0][0] == 'A':
             self.aces += 1
 
     def deal(self, deck):
         self.hit(deck)
         self.hit(deck)
 
-    def ace_value(self):
-        for index, card in enumerate(self.cards):
-            if 'A' in card[index] and card[1] == 11:
+    def ace_value_to_1(self):
+        # check if an Ace is in self.cards and changes it to 1
+        for card in self.cards:
+            if card[1] == 11:
                 card[1] = 1
                 break
 
-#need to add in the Ace check for bust
     def is_blackjack(self):
         if len(self.cards) == 2 and self.point_value == 21:
-            print('Black Jack! \n sum is: %i' % self.point_value)
+            print('Black Jack!')
             return True
         elif self.point_value == 21:
-            print('Black Jack! \n sum is: %i' % self.point_value)
+            print('Black Jack!')
             return True
         elif self.point_value > 21:
-            print('Bust... \n sum is: %i' % self.point_value)
-            print('You loose.')
+            print('Bust...You loose.')
             return True
         else:
             return False
@@ -88,16 +97,19 @@ if __name__ == '__main__':
         game.shuffle()
         player.deal(game.deck)
         while not game_over:
-            print(player.show())
+            player.check_and_show()
+            print(player.check_and_show())
             if player.is_blackjack():
                 game_over = True
                 break
             while raw_input('What would you like to do? (hit,stay) ').lower() != 'stay':
                 player.hit(game.deck)
-                print(player.show())
+                player.check_and_show()
+                print(player.check_and_show())
                 if player.is_blackjack():
                     game_over = True
                     break
+            # do something if stay
         if raw_input('Would you like to play again? \'yes\'? ').lower() == 'yes':
             play_again = True
             game_over = False
--- python_practice/enumerate.py
+++ python_practice/enumerate.py
@@ -1,4 +1,3 @@
-#%%
 def ace_value(a_list):
     for index, item in enumerate(a_list):
         if 'A' in a_list[index] and item[1] == 11:
@@ -14,19 +13,27 @@ def points(a_list):
     return value
 
 
-#%%
 ace_count = 0
 a = [['A', 1], ['A', 11], ['J', 3], ['A', 11]]
 
-#%%
 my_points = points(a)
 points(a)
 
-#%%
 while my_points > 21:
     a = ace_value(a)
     print(a)
     my_points = points(a)
     print(my_points)
+#%%
+def points_v2(cards):
+    enumerate_points = 0
+    for index, card in enumerate(cards):
+        print(cards[index][1])
+
+    for_points = 0
+    for card in cards:
+        print(cards[0][1])
+
+points_v2([['A', 1], ['A', 11], ['J', 3], ['A', 11]])
 
 #%%
