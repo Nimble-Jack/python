e47ed565901732d33278d40d97795b743f1c6c30
nimblejack117@gmail.com <nimblejack117@gmail.com>
2019-09-22 00:09:45 -0700

    no division

--- python_practice/interview_practice/product_of_array_without_position.py
+++ python_practice/interview_practice/product_of_array_without_position.py
@@ -38,3 +38,44 @@ def product_without_i(my_list):
 
 product_without_i([1, 2, 3, 4, 5, 6])
 product_without_i([6, 5, 4, 3, 2, 1])
+
+'''
+without division - 21min
+'''
+def product_of_all_no_division(my_list):
+    total = 1
+    new_list = []
+    for index, num in enumerate(my_list):
+        first_slice = my_list[:index]
+        if len(first_slice) == 0:
+            first_slice.append(1)
+        second_slice = my_list[index+1:]
+        if len(second_slice) == 0:
+            second_slice.append(1)
+        first = 1
+        for num in first_slice:
+            first *= num
+        second = 1
+        for num in second_slice:
+            second *= num
+        total = first * second
+        new_list.append(total)
+    return new_list
+
+product_of_all_no_division([3,4,5])
+
+#%%
+def simple(my_list):
+    new_array = []
+    for index, num in enumerate(my_list):
+        total = 1
+        current_value = my_list.pop(index)
+        for num2 in my_list:
+            total *= num2
+        my_list.insert(index,current_value)
+        new_array.append(total)
+    return new_array
+
+simple([3,4,5])
+
+#%%
